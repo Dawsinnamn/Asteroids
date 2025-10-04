6d0915b9b1091f7823d61919feb2964aed8c19fc
Dawsinnamon <dawson.s.francis@gmail.com>
2025-10-04 12:17:48 -0700

    Added Circleshape.py and player.py. Player class properly initialized and called into main.py

--- Play_Asteroids/main.py
+++ Play_Asteroids/main.py
@@ -1,11 +1,17 @@
 import pygame
 from constants import *
+from player import *
+
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = clock.tick(60)/1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     game_run = True
     while game_run:
@@ -14,9 +20,9 @@ def main():
                 return
 
         screen.fill("black")
-        #screen.flip()
+        player.draw(screen)
         pygame.display.flip()
-
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
